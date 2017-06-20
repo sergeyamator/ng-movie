fd5152470e8db70bf27b44e6093083830476ab33
Serhii Rubets <serhii_rubets@epam.com>
2017-06-20 13:24:05 +0300

    First test was added

--- src/searchBar/search-bar.test.js
+++ src/searchBar/search-bar.test.js
@@ -1,22 +1,25 @@
 import app from '../app/app-module';
+import SearchBarController from './searchBarController';
 
 describe('Search Bar Controller', () => {
   let sut;
 
   beforeEach(() => {
-    angular.module('app');
+    angular.mock.module(app);
   });
 
 
   beforeEach(angular.mock.inject((_$controller_) => {
     // The injector unwraps the underscores (_) from around the parameter names when matching
-    sut = _$controller_('SearchBarController', {
-      $scope: {},
-    });
+    sut = _$controller_(SearchBarController, {
+          $scope: {},
+      });
   }));
 
-  it('should ', () => {
-    const title = 'longerthaneightchars';
-    expect(title).toEqual(title);
+  it('#clearSearchText ', () => {
+    sut.searchText = 'longerthaneightchars';
+    sut.clearSearchText();
+
+    expect(sut.searchText).toEqual('');
   });
 });
