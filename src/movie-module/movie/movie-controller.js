f5c04684d7f005f02f36e25681debc4e8f364ccc
sergey <sramator@gmail.com>
2017-06-27 21:51:24 +0300

    Movie template was started

--- src/movie-module/movie/movie-controller.js
+++ src/movie-module/movie/movie-controller.js
@@ -7,7 +7,28 @@ class Movie {
 
   constructor($stateParams, movie) {
     this.movieId = $stateParams.movieId;
-    movie.getVideoById(this.movieId).then(q => console.log(q));
+    movie.getVideoById(this.movieId).then(r => this.__read(r));
+  }
+
+  __read(movieData) {
+    const genres = movieData.genres.map(genre => genre.name);
+
+    this.movie = {
+      country: movieData.origin_country,
+      date: movieData.first_air_date,
+      imgSrc: movieData.backdrop_path || movieData.poster_path,
+      homepageUrl: movieData.homepage,
+      id: movieData.id,
+      language: movieData.original_language,
+      numberOfEpisodes: movieData.number_of_episodes,
+      number_of_seasons: movieData.number_of_seasons,
+      overview: movieData.overview,
+      popularity: movieData.popularity,
+      title: movieData.original_name,
+      genres,
+    };
+
+    console.log(this.movie);
   }
 }
 
